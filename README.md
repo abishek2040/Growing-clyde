901ba562af8760a4f899d6cfc5e2c48c2cb44455
Ubuntu <ubuntu@ip-172-31-23-141.ec2.internal>
2024-01-08 15:31:15 +0000

    My first commit

--- README.md
+++
@@ -1,7 +0,0 @@
-# Growing-clyde
-Test / Demo - Git Commands
-
-A document that gives you a basic idea of what this repo is about. 
-
-This is a public repo. 
-
---
+++ hello.py
@@ -0,0 +1,7 @@
+# This is a basic python file, that takes in user name and greets the user. 
+
+name = input("What is your name: ") 
+
+print(f"Hello {name}, hope you are doing well.")
+
+
