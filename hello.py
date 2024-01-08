710adfb93106e3975222140cc1932ab5dd58d81f
Ubuntu <ubuntu@ip-172-31-23-141.ec2.internal>
2024-01-08 23:19:10 +0000

    Added a function that takes in names and prints out full names.

--- hello.py
+++ hello.py
@@ -8,3 +8,17 @@ result = addition(10, 20)
 print(result)
 
 
+
+# Here we are adding a function that asks the user for their name and surname and prints out their full name. 
+
+f_name = input("Enter your first name: ") 
+l_name = input("Enter your last name: ")
+
+def full_name():
+    """Prints the full name."""
+    full_name = f_name + " " + l_name
+    return full_name
+
+full = full_name()
+print(full) 
+
