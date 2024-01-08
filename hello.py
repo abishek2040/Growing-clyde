c9347cfc89939b0eddee047c0dbb8cd5da56e81c
Ubuntu <ubuntu@ip-172-31-23-141.ec2.internal>
2024-01-08 16:03:31 +0000

    A new function that adds 2 numbers

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-# Deleted everything in this file. 
+# Deleted everything in this file.
+
+# Added the add function:
+
+def addition(num1, num2):
+    add = num1 + num2 
+    return add
+
+result = addition(10, 20) 
+print(result)
+
+
